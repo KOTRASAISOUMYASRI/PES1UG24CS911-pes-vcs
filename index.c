2aee394e0c1e0930c6a25c43ddc762cb4cf41502
KOTRASAISOUMYASRI <kotrasaisoumyasri@gmail.com>
2026-04-20 20:48:25 +0530

    Update index.c

--- index.c
+++ index.c
@@ -135,6 +135,30 @@ int index_status(const Index *index) {
 //
 // Returns 0 on success, -1 on error.
 
+int index_load(Index *index) {
+    index->count = 0;
+
+    FILE *f = fopen(INDEX_FILE, "r");
+    if (!f) return 0; // empty index
+
+    while (index->count < MAX_INDEX_ENTRIES) {
+        IndexEntry *e = &index->entries[index->count];
+
+        char hash_hex[HASH_HEX_SIZE + 1];
+
+        if (fscanf(f, "%o %64s %ld %zu %[^\n]\n",
+                   &e->mode, hash_hex, &e->mtime_sec,
+                   &e->size, e->path) != 5)
+            break;
+
+        hex_to_hash(hash_hex, &e->hash);
+        index->count++;
+    }
+
+    fclose(f);
+    return 0;
+}
+
 // Save the index to .pes/index atomically.
 //
 // HINTS - Useful functions and syscalls:
